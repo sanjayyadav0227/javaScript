7898d32ab5a8f8bb89ada0d7d6161e20115cc51d
sanjayyadav0227 <sanjaykumaryadav0227@gmail.com>
2025-08-12 14:29:21 +0530

    51-70

--- jsPractice.js
+++ jsPractice.js
@@ -672,3 +672,339 @@ ___________
 
 obj constant hone ke karan pura obj change nhi kr sakte sidha obj ok change nhi kr sakte obj. krke change kr sakte h
 =================================================
+51.
+for (let i = 0; i < 3; i++) {
+  (function() {
+    console.log(i);
+  })();
+}
+
+
+Question: What will be printed and why? Analyze the behavior of IIFE (Immediately Invoked Function Expression) with `let` in loops.
+-------------------------
+0  1  2  
+_________
+for loop 0 se 2 tak chalega or 3 pr false ho jayega for loop me iife function h es kiye ye i ke value print kr rha h jitani bar loop chalege utani bat iife call hoga
+====================================================
+52.
+function test() {
+  console.log(foo);
+  var foo = 10;
+  function foo() {}
+  console.log(foo);
+}
+test();
+
+
+Question: What will be the output and why? Explain the order in which function declarations and variable declarations are hoisted.
+
+-----------------------------
+[function: foo]
+10
+______________
+esme ek hi name ka variable and function h or ye dono hi hoist hoge pr function me function foo(){} jayega and variable me var foo; es karan pahale vale log me function jayega and bad vale log me variable,
+=================================================
+var x = 5;
+function scopeCheck() {
+  console.log(x);
+  var x = 10;
+  console.log(x);
+}
+scopeCheck();
+
+
+Question: What will be printed and why? Discuss how variable hoisting affects the `x` variable within the function.
+----------------------
+undefined  10
+_______________
+scope
+log function me hone ke karan function vala x hoist hoga or fir log me undefined hoga or fir dusare log se pale x initialization ho gya h esa liye 10 print hoga. 
+===============================================
+54.
+function counter() {
+  counter.count = (counter.count || 0) + 1;
+  return counter.count;
+}
+
+console.log(counter());
+console.log(counter());
+
+
+Question: What will be the output? How does the function property `counter.count` affect the closure and variable persistence?
+-------------------------------
+1  2
+__________
+js me function ko object hi mana jata h es liye ham count key me kuch assign kr rhe h , (counter.count || 0) es condition me pahale to count undefined hone se 0 ayega or 0+1 =1 count me assign ho jayega fir bad me count me 1 hone ke karan 1+1=2 hoga 
+====================================================
+55.
+let x = 10;
+{
+  let x = 20;
+  var y = 30;
+}
+console.log(x);
+console.log(y);
+
+
+Question: What will be the output? Explain how `let` and `var` behave differently inside block scope.
+-----------------------------
+10  30
+____________
+let block scope hone ke karan let x =20; block me access hoga bahar nhi es liye log x me 10 aayega and var function scope hone ke karan var GEC me hi ho es liye log y me 30 print hoga
+===================================================
+56.
+console.log(func());
+var func = function() {
+  return 10;
+};
+
+
+Question: What will be printed and why? Discuss the difference in hoisting behavior between function declarations and function expressions.
+-------------------------------
+type error
+____________
+esme func ek function nhi h ye ek expression h es liye type error aayega.
+====================================================
+const a = 5;
+{
+  const a = 10;
+  console.log(a);
+}
+console.log(a);
+
+
+Question: What will be printed? Discuss how `const` works in different block scopes and whether re-declaration is possible.
+--------------------------------
+10   5
+________
+esme const a= 5 hone ke karan es block me hi h ye dusara a h jo es block me hi access kiya ja sakta h esa liye under log me 10 and bahar log me 5 print hoga
+===================================================
+58.
+(function() {
+  console.log(a);
+  var a = 10;
+})();
+
+
+Question: What will be the output and why? Explain the role of hoisting inside an IIFE.
+------------------------------------
+undefined
+___________
+esme iife function me hoisting ho rhi h, a top pr hoist ho jayega and declare usi line pr hoga es liye log me undefined hoga
+================================================
+59.
+function outer() {
+  let counter = 0;
+  return function increment() {
+    counter++;
+    return counter;
+  };
+}
+
+let increment1 = outer();
+let increment2 = outer();
+console.log(increment1());
+console.log(increment2());
+console.log(increment1());
+
+Question: What will be printed? Discuss how closures preserve their lexical environment across different instances of the same function.
+----------------------------
+1   1   2
+_____________
+esme outer ko two ler me dal diya yani outer ke two expresstion ban gye h dono me counter 0 h jese hi increment1 se call kiya to increment1 me counter ki value post increment hokr 1 print ho gyi, and ab increment2 se call kiya to pahale esme counter ki value 0 thi fir post increment hokr 1 ho gyi, and ab
+increment1 se vapash call kiya gya to counter ki value 1 se post increment hokr 2 ho gyi
+==================================================
+
+60.
+for (var i = 0; i < 3; i++) {
+  let j = i;
+  console.log(j);
+}
+console.log(i);
+
+
+Question: What will be printed and why? Explain the difference between `var` and `let` in loop variable declaration.
+-----------------------------------
+0   1    2    3
+__________________
+esme loop 0 se start ho rha h or 2 tak chal rah h or loop me i ko j me assign kiya ja rha h or j ko log kiya ja raha h to 0  1  2 print ho rha h, and i ki value increce hokr 3 hone ke karan loop me condition false ho gye or i me last value 3 raha gye loop se bahar hone pr i ko log kiya ja rha h to fir 3 print ho rha h
+==================================================
+61.
+let a = 5;
+function test() {
+  var a = 10;
+  console.log(a);
+}
+test();
+console.log(a);
+
+
+Question: What will be printed and why? Discuss how `let` and `var` interact in terms of scope and shadowing.
+
+---------------------------------
+10    5
+____________
+var function scope hone ke karan var test function me hi khatam ho jayega or let GEC me hone ke karan sabhi jagah use hoga
+====================================================
+62.
+function testScope() {
+  console.log(x);
+  var x = 10;
+  console.log(x);
+}
+testScope();
+
+
+Question: What will be printed? Analyze how `var` variables are hoisted within a function and how their value is handled before initialization.
+
+------------------------------
+undefined   10
+________________
+yha hoisting ke karan pahale log me undefined aayega and bad me 10 aayega
+====================================================
+63.
+
+var a = 10;
+function testVar() {
+  var a = 20;
+  console.log(a);
+}
+testVar();
+console.log(a);
+
+
+Question: What will be the output? Explain how variable scope works when re-declaring `var` inside a function.
+
+--------------------------------------
+20   10
+___________
+var function scope hone ke karan var testVar function me hi khatam ho jayega or var a = 10; GEC me hone ke karan sabhi jagah use hoga
+======================================================
+64.
+function outerFunc() {
+  var x = 10;
+  function innerFunc() {
+    console.log(x);
+  }
+  innerFunc();
+}
+outerFunc();
+
+
+Question: What will be printed and why? Discuss the concept of lexical scoping in nested functions.
+
+--------------------------
+ 10
+____
+inner function ka x globel hone ke karan inner function ke log me 10 print ho jayega
+=====================================================
+65.
+let a = 5;
+{
+  let a = 10;
+  console.log(a);
+}
+console.log(a);
+
+
+Question: What will be printed? Discuss how block scoping with `let` affects variable visibility inside and outside the block.
+
+----------------------------
+10   5
+_________
+let block scope hone ke karan a=10; block me khatam ho jayega or let a =5; GEC me hone ke karan pure code me use kiya ja sakta h
+=====================================================
+66.
+function outerFunc() {
+  let count = 0;
+  return {
+    increment: function() {
+      count++;
+      return count;
+    },
+    decrement: function() {
+      count--;
+      return count;
+    }
+  };
+}
+
+let counter = outerFunc();
+console.log(counter.increment());
+console.log(counter.decrement());
+
+
+Question: What will be the output and why? Analyze how closures behave with multiple functions accessing the same lexical scope.
+---------------------------------
+1   0
+_______
+outer function ko counter me store kiya and couner ko expression banaya gya or fir counter se increment function ko call kiya to count post increment ho gya or 1 print kiya , fir counter se decrement call kiya gya or count ki value 1 se decrece hokar vapas 0 ho gyi
+====================================================
+67.
+console.log(test());
+function test() {
+  return 5;
+}
+
+
+Question: What will be printed and why? Explain how function declarations are hoisted.
+
+-------------------------------
+hoisting ke karan test function ka pura scope top pr jayega or log me 5 print ho jayega
+=====================================================
+
+68.
+console.log(foo());
+var foo = function() {
+  return 5;
+};
+
+
+Question: What will happen here and why? Discuss the difference between function expressions and function declarations in terms of hoisting.
+
+--------------------------
+typeError
+___________
+foo ek function na hone ke karn type error aayega foo yha pr expression h
+=====================================================
+69.
+let a = 1;
+function outer() {
+  let b = 2;
+  function inner() {
+    console.log(a, b);
+  }
+  inner();
+}
+outer();
+
+
+Question: What will be printed and why? Explain how the scope chain works in nested functions.
+---------------------------------
+1    2
+_______
+a GEC me hone ke karan function ke under use kiya ja sakta h or b inner ka globel hone ke karan inner me use kiya ja raha h esa karan 1   2 print honge.
+=====================================================
+70.
+function outer() {
+  let x = 10;
+  return function() {
+    return x++;
+  };
+}
+
+const a = outer();
+const b = outer();
+
+console.log(a());
+console.log(b());
+console.log(a());
+console.log(b());
+
+Question: What will be printed? Discuss how closures work with multiple instances and how they preserve their own state.
+------------------------------
+10   10   11   11
+___________________
+esme outer ko two const me dal diya yani outer ke two expresstion ban gye h dono me x 10 h jese hi a se call kiya to x post increment hote in return kr diya to 10 return hoga and fir b call kiya to esme bhi x ki value 10 h or return me post increment 	hone ke karan 10 hi return ho jayega fir vagash se a ko call kiya gya h to x ki value 11 h to return me post increment hone ke karan 11 hi return hoga , fir b call ho raha b me bhi x ki value 11 h or return me post increment hone ke karan 11 hi log loga						
+=====================================================
+
